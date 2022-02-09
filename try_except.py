241f428d069f995be62457d099b93284a785234f
avtandil zabakhidze <98325117+avtandilzabakhidze@users.noreply.github.com>
2022-02-09 09:16:02 -0800

    Add files via upload

---
+++ try_except.py
@@ -0,0 +1,57 @@
+#1
+
+
+# try:
+#     x = int(input("enter x"))
+# except:
+#     x = 10
+# try:
+#     y = int(input("enter y"))
+# except:
+#     y = 1
+
+# 2 ---------------------------
+
+
+# from pkg_resources import yield_lines
+
+
+# x = 4
+# y = "sdfgsdfg"
+
+# try :
+#     print(x/y)
+# except TypeError:
+#     try:
+#         y = int(y)
+#         print(x/y)
+#     except ValueError:
+#         print(0)
+
+
+# 3 ----------------
+
+
+# from tkinter import EXCEPTION
+# def cube(x):
+#     try:
+#         x = int(x)
+#         return x**3
+#     except Exception as e:
+#         print(e)
+#         return 0
+# print(cube("asdas"))
+
+# 4
+# ---------------------
+# x = 10 
+# y =5
+
+# try:
+#     print(int(x/y))
+# except:
+#     print(0)
+# else:
+#     print(x,y)
+# finally:
+#     print("i don't care")
